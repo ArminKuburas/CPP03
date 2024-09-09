cdd55d0cc7407156bf4057aff6a7ba78394f3cc4
ArminKuburas <akuburass@gmail.com>
2024-09-09 19:27:02 +0300

    feat: Exercise 00 added

---
+++ ex00/ClapTrap.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 19:05:57 by akuburas          #+#    #+#             */
+/*   Updated: 2024/09/09 19:18:16 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(const std::string& name) : _name(name), _hitpoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap " << this->_name << " has been created!" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& source) : _name(source._name), _hitpoints(source._hitpoints), _energyPoints(source._energyPoints), _attackDamage(source._attackDamage)
+{
+	std::cout << "ClapTrap " << this->_name << " has been copied!" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << this->_name << " has been destroyed!" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+	if (this != &other)
+	{
+		std::cout << "ClapTrap " << this->_name << " is now " << other._name << std::endl;
+		this->_name = other._name;
+		this->_hitpoints = other._hitpoints;
+		this->_energyPoints = other._energyPoints;
+		this->_attackDamage = other._attackDamage;
+	}
+	return (*this);
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->_energyPoints > 0 && this->_hitpoints > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << " causing " << this->_attackDamage << " points of damage!" << std::endl;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " is out of energy points or out of hit points!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hitpoints > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
+		this->_hitpoints -= amount;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " is already out of hitpoints!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_hitpoints > 0 && this->_energyPoints > 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " is being repaired for " << amount << " points!" << std::endl;
+		this->_hitpoints += amount;
+		this->_energyPoints--;
+	}
+	else
+		std::cout << "ClapTrap " << this->_name << " is out of energy points or out of hit points!" << std::endl;
+}
+
---
+++ ex00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 19:22:22 by akuburas          #+#    #+#             */
+/*   Updated: 2024/09/09 19:25:47 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap a("Clap my Trap");
+	ClapTrap b("Clap Whos Trap?");
+	ClapTrap c(a);
+	ClapTrap d("ClapTrap");
+
+	d = b;
+
+	a.attack("Clap Whos Trap?");
+	b.takeDamage(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	c.beRepaired(5);
+	d.attack("Clap my Trap");
+	a.takeDamage(5);
+	d.attack("Clap my Trap");
+	a.takeDamage(5);
+	d.attack("Clap my Trap");
+	a.takeDamage(5);
+	return (0);
+}
